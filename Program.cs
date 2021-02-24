5f23c27161a512934a9f530fdd4350eb5a21c571
Matt Leslie <mrossleslie@users.noreply.github.com>
2021-02-24 14:24:56 -0600

    Leslie - Refactored Program.cs, Deprecated Newtonsoft from dependencies, Hooking into ReadyAsync, Moved Program.cs to base

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace MASBot
+{
+	public class Program
+	{
+		private readonly DiscordSocketClient _client;
+		private readonly IConfiguration _config;
+
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public Program()
+		{
+			_client = new DiscordSocketClient();
+			_client.Ready += ReadyAsync;
+			_client.Log += Log;
+
+			var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile(path: "config.json");
+			_config = builder.Build();
+		}
+
+		public async Task MainAsync()
+		{
+			await _client.LoginAsync(TokenType.Bot, _config["Token"]);
+			await _client.StartAsync();
+
+			await Task.Delay(-1);
+		}
+		private Task ReadyAsync()
+		{
+			Console.WriteLine($"Connected as -> [] :)");
+			return Task.CompletedTask;
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
